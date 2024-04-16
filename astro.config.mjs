7fd418273c6b3edd9729a508a3d37b73aaf95382
jeong99 <jbh990621@gmail.com>
2024-04-16 16:01:36 +0900

    preact 아일랜드 컴포넌트 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://jeong99-astro-tutorial.netlify.app/",
+  integrations: [tailwind(), preact()]
 });
